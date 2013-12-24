d429df8d54952ec769d320918e9b27d185417f67
Austin W Ellis <austinellis+git@gmail.com>
2013-12-24 14:02:01 -0800

    added better error messages

--- build.py
+++ build.py
@@ -5,31 +5,43 @@ import sublime, sublime_plugin
 class RunPythonTestCommand(sublime_plugin.TextCommand):
     def run(self, edit):
 
+
+        REPO_NAME = '/mightyspring/'
         abs_file = self.view.file_name()
-        self.test_root = abs_file.split('mightyspring')[0] + 'mightyspring/'
+        folders = abs_file.lower().split(REPO_NAME)
 
-        commands = [
-            'cd %s' % self.test_root,
-            'source venv/bin/activate',
-            'python scent.py tests --stop',
-            'deactivate',
-        ]
+        if len(folders) > 1:
+            REPO_ROOT = folders[0] + REPO_NAME
 
+            commands = [
+                'echo "Running tests, Sir..." ',
+                'cd %s' % REPO_ROOT,
+                'source venv/bin/activate',
+                'python scent.py tests --stop',
+                'deactivate',
+            ]
+            
+            self.view.window().run_command(
+                'exec',
+                {
+                    'cmd': [';'.join(commands)],
+                    'shell': True,
+                    'working_dir': REPO_ROOT,
+                }
+            )
+        else:
+            #if the user isn't in the right directory, print a message to the console.
+            commands = [
+                'echo "Sir, there was an error running Mighty Spring tests."',
+                'echo "Please navigate to a file in the MightySpring repositiory and re-run this command."',
+            ]
+                
+            self.view.window().run_command(
+                'exec',
+                {
+                    'cmd': [';'.join(commands)],
+                    'shell': True,
+                    'working_dir': '',
+                }
+            )
 
-        self.view.window().run_command(
-            'exec',
-            {
-                'cmd': [';'.join(commands)],
-                'shell': True,
-                'working_dir': self.test_root,
-            }
-        )
-
-
-        '''
-        v = self.view.window().get_output_panel('exec')
-        edit = v.begin_edit()
-        v.insert(edit, v.size(), 'Running Tests...\n')
-        v.end_edit(edit)
-        v.show(v.size())
-        '''
