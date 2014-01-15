e9eb12238d8fed8b4531fa6d60b119b6d85c0ee7
Austin W Ellis <austinellis+git@gmail.com>
2014-01-14 17:12:27 -0800

    if you have a test open, only run tests in that folder

--- build.py
+++ build.py
@@ -13,13 +13,23 @@ class RunPythonTestCommand(sublime_plugin.TextCommand):
         if len(folders) > 1:
             REPO_ROOT = folders[0] + REPO_NAME
 
-            commands = [
-                'echo "Running tests, Sir..." ',
-                'cd %s' % REPO_ROOT,
-                'source venv/bin/activate',
-                'python scent.py tests --stop',
-                'deactivate',
-            ]
+            if 'tests' in folders[1]:
+                test_folders = '/'.join(folders[1].split('/')[:-1])
+                commands = [
+                    'echo "Running tests in %s, Sir...\nTo run all tests, navigate to a file outside of the \'tests\' directory." ' % test_folders,
+                    'cd %s' % REPO_ROOT,
+                    'source venv/bin/activate',
+                    'python scent.py %s --stop' % test_folders,
+                    'deactivate',
+                ]
+            else:
+                commands = [
+                    'echo "Running tests, Sir..." ',
+                    'cd %s' % REPO_ROOT,
+                    'source venv/bin/activate',
+                    'python scent.py tests --stop',
+                    'deactivate',
+                ]
             
             self.view.window().run_command(
                 'exec',
